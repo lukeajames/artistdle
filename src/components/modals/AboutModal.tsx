2d1d37da8c33e107e13b4a5b7e4f5029305a7f78
lukeajames <97810846+lukeajames@users.noreply.github.com>
2022-02-13 19:28:23 +0000

    Update AboutModal.tsx

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -13,8 +13,7 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          open source</a>
-        version of the word guessing game we all know and
+          open source</a> version of the word guessing game we all know and
         love, adapted to feature the names of musical artists.      
         Answers are either their first or last name. Good luck!
       </p>
