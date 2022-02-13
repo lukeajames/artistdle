befb31c0ee6dcac8ed61c8384758cabe3535e24b
lukeajames <97810846+lukeajames@users.noreply.github.com>
2022-02-13 19:07:55 +0000

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -13,6 +13,10 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
         Guess the word in 6 tries. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
+      
+      <p>
+        All answers are either the first or last name of a popular music artist.
+      </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="A" status="correct" />
